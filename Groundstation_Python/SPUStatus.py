a72ea2d44c0814d85f373a0d6d81cb08055504bf
Robin Grimsmann <robin.grimsmann@gmail.com>
2022-01-12 19:56:05 +0100

    Merged local changed

--- Groundstation_Python/SPUStatus.py
+++ Groundstation_Python/SPUStatus.py
@@ -71,29 +71,140 @@ def GetNumberOfResyncs(statusRegValue):
     mask =  0x000ff000
     return statusRegValue & mask
 
-def LogStatus(syncstatusreg1):
-    SR1_STAMP1_NewAvialable_MISSING 
-    SR1_STAMP2_NewAvialable_MISSING 
-    SR1_STAMP3_NewAvialable_MISSING 
-    SR1_STAMP4_NewAvialable_MISSING 
-    SR1_STAMP5_NewAvialable_MISSING 
-    SR1_STAMP6_NewAvialable_MISSING 
+class Telemetry():
+    def __init__(self):
+        self.timestamp = 0
+        self.STAMP11 = 0
+        self.STAMP12 = 0
 
-    SR1_STAMP1_ReqeustResync
-    SR1_STAMP2_ReqeustResync
-    SR1_STAMP3_ReqeustResync
-    SR1_STAMP4_ReqeustResync
-    SR1_STAMP5_ReqeustResync 
-    SR1_STAMP6_ReqeustResync 
+        self.STAMP21 = 0
+        self.STAMP22 = 0
 
-    SR1_STAMP1_OverflowMarker
-    SR1_STAMP2_OverflowMarker
-    SR1_STAMP3_OverflowMarker
-    SR1_STAMP4_OverflowMarker
-    SR1_STAMP5_OverflowMarker
-    SR1_STAMP6_OverflowMarker
+        self.STAMP31 = 0
+        self.STAMP32 = 0
 
-    SR1_APB_ADDRESS_ERROR
+        self.STAMP41 = 0
+        self.STAMP42 = 0
 
-    SR1_PENDING_READING_INTERRUPT
-    SR1_PENDING_SYNC_INTERRUPT
+        self.STAMP51 = 0
+        self.STAMP52 = 0
+
+        self.STAMP61 = 0
+        self.STAMP62 = 0
+
+        self.StatusReg1 =  0
+        self.StatusReg2 = 0
+    
+    def SetValue(self, value, offset):
+        if (offset == 0):
+            self.timestamp = value
+        elif (offset == 1):
+            self.STAMP11 = value 
+        elif (offset == 2):
+            self.STAMP12 = value 
+        elif (offset == 3):
+            self.STAMP21 = value 
+        elif (offset == 4):
+            self.STAMP22 = value 
+        elif (offset == 5):
+            self.STAMP31 = value
+        elif (offset == 6):
+            self.STAMP32 = value 
+        elif (offset == 7):
+            self.STAMP41 = value
+        elif (offset == 8):
+            self.STAMP42 = value
+        elif (offset == 9):
+            self.STAMP51 = value
+        elif (offset == 10):
+            self.STAMP52 = value
+        elif (offset == 11):
+            self.STAMP61 = value
+        elif (offset == 12):
+            self.STAMP62 = value
+        elif (offset == 13):
+            self.StatusReg1 = value
+        elif (offset == 14):
+            self.StatusReg2 = value
+
+    def ParseBuffer(self, buffer):
+        offset = 0
+        for i in range(4, len(buffer), 4):
+            self.SetValue(int.from_bytes(buffer[i - 4: i], "little"), offset)
+            offset = offset + 1
+    
+    def MeaMeaningOfStatusReg1(self):
+        retStr = ""
+        if (self.StatusReg1 & SR1_STAMP1_NewAvialable_MISSING):
+            retStr = retStr + "(S1 Missing) "
+        if (self.StatusReg1 & SR1_STAMP2_NewAvialable_MISSING):
+            retStr = retStr + "(S2 Missing) "
+        if (self.StatusReg1 & SR1_STAMP3_NewAvialable_MISSING):
+            retStr = retStr + "(S3 Missing) "
+        if (self.StatusReg1 & SR1_STAMP4_NewAvialable_MISSING):
+            retStr = retStr + "(S4 Missing) "
+        if (self.StatusReg1 & SR1_STAMP5_NewAvialable_MISSING):
+            retStr = retStr + "(S5 Missing) "
+        if (self.StatusReg1 & SR1_STAMP6_NewAvialable_MISSING):
+            retStr = retStr + "(S6 Missing) "
+
+        #Resync Markers
+        if (self.StatusReg1 & SR1_STAMP1_ReqeustResync):
+            retStr = retStr + "(S1 Request Resync) "
+        if (self.StatusReg1 & SR1_STAMP2_ReqeustResync):
+            retStr = retStr + "(S2 Request Resync) "
+        if (self.StatusReg1 & SR1_STAMP3_ReqeustResync):
+            retStr = retStr + "(S3 Request Resync) "
+        if (self.StatusReg1 & SR1_STAMP4_ReqeustResync):
+            retStr = retStr + "(S4 Request Resync) "
+        if (self.StatusReg1 & SR1_STAMP5_ReqeustResync):
+            retStr = retStr + "(S5 Request Resync) "
+        if (self.StatusReg1 & SR1_STAMP6_ReqeustResync):
+            retStr = retStr + "(S6 Request Resync) "
+        #Overflow
+        if (self.StatusReg1 & (SR1_STAMP1_OverflowMarker)):
+            retStr = retStr + "(S1 Overflow) "
+        if (self.StatusReg1 & (SR1_STAMP2_OverflowMarker)):
+            retStr = retStr + "(S2 Overflow) "
+        if (self.StatusReg1 & (SR1_STAMP3_OverflowMarker)):
+            retStr = retStr + "(S3 Overflow) "
+        if (self.StatusReg1 & (SR1_STAMP4_OverflowMarker)):
+            retStr = retStr + "(S4 Overflow) "
+        if (self.StatusReg1 & (SR1_STAMP5_OverflowMarker)):
+            retStr = retStr + "(S5 Overflow) "
+        if (self.StatusReg1 & (SR1_STAMP6_OverflowMarker)):
+            retStr = retStr + "(S6 Overflow) "
+        #SODS / LO / SOE / Address error
+        if (self.StatusReg1 & SR1_SIGNAL_LO):
+            retStr = retStr + "(LO) "
+        if (self.StatusReg1 & SR1_SIGNAL_SODS):
+            retStr = retStr + "(SODS) "
+        if (self.StatusReg1 & SR1_SIGNAL_SOE):
+            retStr = retStr + "(SOE) "
+        if (self.StatusReg1 & SR1_APB_ADDRESS_ERROR):
+            retStr = retStr + "(AE) "
+        #Pending Interrupts
+        if (self.StatusReg1 & SR1_PENDING_READING_INTERRUPT):
+            retStr = retStr + "(PRI) "
+        if (self.StatusReg1 & SR1_PENDING_SYNC_INTERRUPT):
+            retStr = retStr + "(PSI) "
+        return retStr
+
+    def Print(self):
+        print("TimeStamp: {}".format(hex(self.timestamp)))
+        print("STAMP1 SGR1: {}".format(hex(self.STAMP11)))
+        print("STAMP1 SGR2: {}".format(hex(self.STAMP12)))
+        print("STAMP2 SGR1: {}".format(hex(self.STAMP21)))
+        print("STAMP2 SGR2: {}".format(hex(self.STAMP22)))
+        print("STAMP3 SGR1: {}".format(hex(self.STAMP31)))
+        print("STAMP3 SGR2: {}".format(hex(self.STAMP32)))
+        print("STAMP4 SGR1: {}".format(hex(self.STAMP41)))
+        print("STAMP4 SGR2: {}".format(hex(self.STAMP42)))
+        print("STAMP5 SGR1: {}".format(hex(self.STAMP51)))
+        print("STAMP5 SGR2: {}".format(hex(self.STAMP52)))
+        print("STAMP6 SGR1: {}".format(hex(self.STAMP61)))
+        print("STAMP6 SGR2: {}".format(hex(self.STAMP62)))
+        print("Status Register 1: {} {}".format(hex(self.StatusReg1), self.MeaMeaningOfStatusReg1()))
+        print("Status Register 2: {}".format(hex(self.StatusReg2)))
+
+       
